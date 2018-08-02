2f614d2d0df93406dde54b1c858134af9dabe82b
scott <1170636+scottcs@users.noreply.github.com>
2018-08-02 11:54:59 -0700

    Move server code back into single file

--- game/server.py
+++ game/server.py
@@ -1,12 +1,50 @@
-"""Game websocket class."""
+"""Autochthon server."""
 import json
-from typing import Any
+from typing import Optional, Any
 
+import tornado.ioloop
+import tornado.web
 import tornado.websocket
 
 from game.events import InputEvent, WebsocketWriteAllEvent
-from game.server.gamecallback import GameCallback
+from game.game import Game, MOMENTS_PER_TURN
+from game.processor.render import WebRenderProcessor
+from game.state import GameState
 from game.types import EventType
+
+DEFAULT_PORT = 19999
+
+
+class MainHandler(tornado.web.RequestHandler):
+    """Main server http handler."""
+
+    def get(self, *args: Any, **kwargs: Any) -> None:
+        """HTTP Get."""
+        self.render('index.html')
+
+
+class GameCallback(tornado.ioloop.PeriodicCallback):
+    """Hook the game loop into tornado's io loop."""
+    game: Optional[Game] = None
+
+    def __init__(self) -> None:
+        ms = 1000 // MOMENTS_PER_TURN
+        if GameCallback.game is None:
+            GameCallback.game = Game(WebRenderProcessor())
+        super().__init__(self.process_events, ms)
+
+    @staticmethod
+    def get_game_state() -> GameState:
+        """state property"""
+        if GameCallback.game is not None:
+            return GameCallback.game.state
+        return GameState.UNKNOWN
+
+    @staticmethod
+    def process_events() -> None:
+        """Send game events to the game."""
+        if GameCallback.game is not None:
+            GameCallback.game.update()
 
 
 class GameWebSocket(tornado.websocket.WebSocketHandler):
@@ -58,3 +96,23 @@ class GameWebSocket(tornado.websocket.WebSocketHandler):
 
         """
         return {}
+
+
+def make_app() -> tornado.web.Application:
+    """Make a new tornado app."""
+    return tornado.web.Application([
+        (r'/', MainHandler),
+        (r'/websocket', GameWebSocket),
+    ],
+        static_path='static',
+        template_path='templates',
+        compress_response=True)
+
+
+def run_server(config: dict) -> None:
+    """Run the game as a websockets server."""
+    port = config['server'].get('port', DEFAULT_PORT)
+    app = make_app()
+    app.listen(port)
+    print(f'Listening on port {port}...')
+    tornado.ioloop.IOLoop.current().start()
