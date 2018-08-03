5ec70871d764aa3f6ea80aef447acab17a04c91d
scott <1170636+scottcs@users.noreply.github.com>
2018-08-03 12:43:30 -0700

    Add Map

---
+++ game/map.py
@@ -0,0 +1,91 @@
+"""Game map."""
+from random import randint
+from typing import List
+
+import numpy as np
+import tcod.map
+
+from game.utils.geometry import Rect, Point
+from game.utils.random import coin_flip
+
+
+class Map(tcod.map.Map):
+    """Game map."""
+
+    def __init__(self, width: int, height: int) -> None:
+        """Create a new map with the given dimensions."""
+        super().__init__(width, height)
+        # self._explored: list = [[False for _ in range(height)] for _ in range(width)]
+        self._buffer2: np.array = np.zeros((height, width, 1), dtype=np.bool_)
+        
+    @property
+    def explored(self) -> np.array:
+        """Array of cells that have been explored."""
+        buffer: np.array = self._buffer2[:, :, 0]
+        return buffer
+
+    def create(self) -> None:
+        """Create the map using the map's algorithm."""
+        raise NotImplementedError('This class must be subclassed.')
+
+
+class ClassicMap(Map):
+    """Classic rogue-style map."""
+
+    def __init__(self, width: int, height: int, config: dict) -> None:
+        super().__init__(width, height)
+        self.max_rooms: int = config.get('max_rooms', 1)
+        self.room_min_size: int = config.get('room_min_size', 3)
+        self.room_max_size: int = config.get('room_max_size', 5)
+
+    def create_room(self, room: Rect) -> None:
+        """Create a new room in the map at the given coordinates."""
+        for x in range(room.p1.x + 1, room.p2.x):
+            for y in range(room.p1.y + 1, room.p2.y):
+                self.walkable[y, x] = True
+                self.transparent[y, x] = True
+
+    def create_h_tunnel(self, x1: int, x2: int, y: int) -> None:
+        """Create a single-width horizontal tunnel from x1 to x2 along y."""
+        for x in range(min(x1, x2), max(x1, x2) + 1):
+            self.walkable[y, x] = True
+            self.transparent[y, x] = True
+
+    def create_v_tunnel(self, y1: int, y2: int, x: int) -> None:
+        """Create a single-width vertical tunnel from y1 to y2 along x."""
+        for y in range(min(y1, y2), max(y1, y2) + 1):
+            self.walkable[y, x] = True
+            self.transparent[y, x] = True
+
+    def create(self) -> None:
+        """Create the map."""
+        rooms: List[Rect] = []
+
+        for r in range(self.max_rooms):
+            w: int = randint(self.room_min_size, self.room_max_size)
+            h: int = randint(self.room_min_size, self.room_max_size)
+            x: int = randint(0, self.width - w - 1)
+            y: int = randint(0, self.height - h - 1)
+
+            new_room: Rect = Rect(x, y, w, h)
+            new_center: Point = new_room.center
+
+            for other_room in rooms:
+                if new_room.intersects(other_room):
+                    break
+            else:  # no intersections, so this room is valid
+                self.create_room(new_room)
+
+                if len(rooms) == 0:  # first room
+                    pass  # TODO: place player here
+                else:
+                    prev_center: Point = rooms[-1].center
+                    if coin_flip():
+                        self.create_h_tunnel(prev_center.x, new_center.x, prev_center.y)
+                        self.create_v_tunnel(prev_center.y, new_center.y, new_center.x)
+                    else:
+                        self.create_v_tunnel(prev_center.y, new_center.y, prev_center.x)
+                        self.create_h_tunnel(prev_center.x, new_center.x, new_center.y)
+
+                # TODO: place entities
+                rooms.append(new_room)
---
+++ game/utils/geometry.py
@@ -0,0 +1,30 @@
+"""Geometry utility classes."""
+from __future__ import annotations
+from typing import NamedTuple
+
+
+class Point(NamedTuple):
+    """Represents a point."""
+
+    x: int
+    y: int
+
+
+class Rect:
+    """Represents a rectangle."""
+
+    def __init__(self, x: int, y: int, w: int, h: int) -> None:
+        self.p1: Point = Point(x, y)
+        self.p2: Point = Point(x + w, y + h)
+
+    @property
+    def center(self) -> Point:
+        """Calculate the center point."""
+        center_x: int = (self.p1.x + self.p2.x) // 2
+        center_y: int = (self.p1.y + self.p2.y) // 2
+        return Point(center_x, center_y)
+
+    def intersects(self, other: Rect) -> bool:
+        """Check whether this rectangle intersects with another one."""
+        return (self.p1.x <= other.p2.x and self.p2.x >= other.p1.x and
+                self.p1.y <= other.p2.y and self.p2.y >= other.p1.y)
