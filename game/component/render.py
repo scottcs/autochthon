1325354cc041ddd8a8f72695a230fbdc11d2d99f
scott <1170636+scottcs@users.noreply.github.com>
2018-09-09 14:40:51 -0700

    Turn Palette(s) into IntEnums

--- game/component/render.py
+++ game/component/render.py
@@ -1,10 +1,11 @@
 """Render components."""
 from game.types import RenderLayer
+from gamedata.palette import Palette
 
 
 class Renderable:
     """Renderable component."""
-    def __init__(self, tile_id: int, tint: int, layer: RenderLayer) -> None:
+    def __init__(self, tile_id: int, tint: Palette, layer: RenderLayer) -> None:
         self.tile_id: int = tile_id
-        self.tint: int = tint
+        self.tint: Palette = tint
         self.layer: RenderLayer = layer
