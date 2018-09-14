e1da936921206fdbbbff8dab54d43e2c6c9fcfaa
scott <1170636+scottcs@users.noreply.github.com>
2018-09-14 12:14:43 -0700

    Remove GameTime; use int instead

--- game/component/action.py
+++ game/component/action.py
@@ -1,23 +1,13 @@
 """Action components."""
-from typing import Union
-
-from game.utils.time import GameTime
-from gamedata.base_engine_values import MOMENTS_PER_TURN
+from gamedata.base_engine_values import TIME_UNITS_PER_TURN
 
 
 class Actor:
     """Actor components use time units of energy to take actions."""
 
-    def __init__(
-        self,
-        initial_units: Union[GameTime, int] = 0,
-        rate: Union[GameTime, int] = MOMENTS_PER_TURN,
-    ) -> None:
-        self.time_units: GameTime = GameTime(initial_units)
-        self.rate: GameTime = GameTime(rate)
+    def __init__(self, initial_units: int = 0, rate: int = TIME_UNITS_PER_TURN) -> None:
+        self.time_units: int = initial_units
+        self.rate: int = rate
 
     def __str__(self) -> str:
-        return (
-            f"{self.time_units} ({self.time_units.last_time}/{self.time_units.last_change}) "
-            f"(+{self.rate}/turn)"
-        )
+        return f"{self.time_units} (+{self.rate}/turn)"
