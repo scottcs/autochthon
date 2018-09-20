130404f5b5bb612439b06861786ccde483ffbb95
scott <1170636+scottcs@users.noreply.github.com>
2018-09-19 22:54:12 -0700

    Add Equipment slots

--- game/component/container.py
+++ game/component/container.py
@@ -24,6 +24,7 @@ class Containable:
 
     def __post__init__(self) -> None:
         self.count: int = 1
+        self.equipped = False
 
 
 @dataclass
@@ -57,3 +58,64 @@ class GUTContainerTransfer:
 
 class Item:
     """An Item."""
+
+
+class Equipment:
+    """Equipment slots."""
+
+    def __init__(
+        self,
+        head_name: str = "head",
+        head_max: int = 1,
+        face_name: str = "face",
+        face_max: int = 1,
+        neck_name: str = "neck",
+        neck_max: int = 1,
+        shoulder_name: str = "shoulders",
+        shoulder_max: int = 1,
+        back_name: str = "back",
+        back_max: int = 1,
+        torso_name: str = "torso",
+        torso_max: int = 1,
+        waist_name: str = "waist",
+        waist_max: int = 1,
+        tail_name: str = "tail",
+        tail_max: int = 0,
+        wrist_name: str = "wrists",
+        wrist_max: int = 2,
+        hand_name: str = "hands",
+        hand_max: int = 1,
+        finger_name: str = "fingers",
+        finger_max: int = 2,
+        leg_name: str = "legs",
+        leg_max: int = 1,
+        foot_name: str = "feet",
+        foot_max: int = 1,
+        implant_name: str = "implant",
+        implant_max: int = 3,
+        shield_name: str = "shield",
+        shield_max: int = 1,
+        melee_name: str = "melee",
+        melee_max: int = 1,
+        ranged_name: str = "ranged",
+        ranged_max: int = 1,
+    ):
+        self.slots = {
+            EquipType.head: {'name': head_name, 'slots': head_max},
+            EquipType.face: {'name': face_name, 'slots': face_max},
+            EquipType.neck: {'name': neck_name, 'slots': neck_max},
+            EquipType.shoulder: {'name': shoulder_name, 'slots': shoulder_max},
+            EquipType.back: {'name': back_name, 'slots': back_max},
+            EquipType.torso: {'name': torso_name, 'slots': torso_max},
+            EquipType.waist: {'name': waist_name, 'slots': waist_max},
+            EquipType.tail: {'name': tail_name, 'slots': tail_max},
+            EquipType.wrist: {'name': wrist_name, 'slots': wrist_max},
+            EquipType.hand: {'name': hand_name, 'slots': hand_max},
+            EquipType.finger: {'name': finger_name, 'slots': finger_max},
+            EquipType.leg: {'name': leg_name, 'slots': leg_max},
+            EquipType.foot: {'name': foot_name, 'slots': foot_max},
+            EquipType.implant: {'name': implant_name, 'slots': implant_max},
+            EquipType.shield: {'name': shield_name, 'slots': shield_max},
+            EquipType.melee: {'name': melee_name, 'slots': melee_max},
+            EquipType.ranged: {'name': ranged_name, 'slots': ranged_max},
+        }
