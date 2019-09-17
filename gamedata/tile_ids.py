7f7c8833a9bd31c3e8aa7c97eb065cff0b65c68c
scott <1170636+scottcs@users.noreply.github.com>
2019-09-16 19:39:56 -0700

    Move tileset choice to config

--- gamedata/tile_ids.py
+++ gamedata/tile_ids.py
@@ -3,7 +3,11 @@ import json
 import pathlib
 import typing
 
-TILE_IDS_FILE = pathlib.Path("data/tiles/oryx_ur/tile_ids.json")
+import gamedata.config
+
+TILE_IDS_FILE = pathlib.Path(
+    f"data/tiles/{gamedata.config.CONFIG['tiles']['tileset']}/tile_ids.json"
+)
 
 with TILE_IDS_FILE.open() as tile_ids_file_handle:
     TILE_IDS: typing.Dict[str, typing.Dict[typing.Any, typing.Any]] = json.load(
