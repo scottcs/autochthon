9958805dcdb29c211fde80c1e02a0a149124ec67
scott <1170636+scottcs@users.noreply.github.com>
2019-09-19 20:06:55 -0700

    Hook up entities

--- game/processor/render.py
+++ game/processor/render.py
@@ -7,9 +7,11 @@ import bearlibterminal.terminal as blt
 import esper
 import tcod
 
+import game.component.container
 import game.component.movement
 import game.component.player
 import game.component.render
+import game.component.status
 import game.const.config
 import game.const.palette
 import game.const.tile_ids
@@ -27,7 +29,6 @@ class BearLibRender(esper.Processor):
     """Game render processor for BearLibTerminal console."""
 
     def __init__(self) -> None:
-        self.render_entities: set = set()
         self.width = game.const.tileset.DATA["window"]["width"]
         self.height = game.const.tileset.DATA["window"]["height"]
 
@@ -55,6 +56,7 @@ class BearLibRender(esper.Processor):
 
     def process(self, *args: typing.Any, **kwargs: typing.Any) -> None:
         """Process all renderables."""
+        # TODO: optimize
         blt.layer(game.types.RenderLayer.background)
         blt.color("white")
 
@@ -64,7 +66,6 @@ class BearLibRender(esper.Processor):
         viewport_x = player_data.x - center_x
         viewport_y = player_data.y - center_y
 
-        # TODO: fov
         self.world.map.compute_fov(
             player_data.x,
             player_data.y,
@@ -72,10 +73,77 @@ class BearLibRender(esper.Processor):
             radius=player_data.fov,
             light_walls=True,
         )
-        # TODO: enemies
-        # TODO: items
-        # TODO: optimize
 
+        self._draw_map(viewport_x, viewport_y)
+        self._draw_entities(viewport_x, viewport_y)
+        self._draw_player(center_x, center_y, player_data)
+
+        blt.refresh()
+
+    @staticmethod
+    def _draw_player(center_x, center_y, player_data):
+        blt.layer(player_data.layer)
+        blt.color(player_data.color)
+        blt.put(center_x, center_y, player_data.tile_id)
+
+    def _draw_entities(self, viewport_x, viewport_y):
+        render_entities = set()
+        for ent, renderable in self.world.get_component(game.component.render.Renderable):
+            position = self.world.optional_component_for_entity(
+                ent, game.component.movement.Position
+            )
+            if position is not None:
+                if self.world.map.fov[position.y, position.x]:
+                    # we can see it now
+                    render_entities.add((renderable.layer.value, ent))
+            if renderable.last_seen_x is not None:
+                # we've seen it before
+                render_entities.add((renderable.layer.value, ent))
+
+        for _, ent in sorted(render_entities):
+            renderable = self.world.component_for_entity(ent, game.component.render.Renderable)
+            position = self.world.optional_component_for_entity(
+                ent, game.component.movement.Position
+            )
+            # is_dead = self.world.has_component(ent, game.component.status.GUTDead)
+            # is_contained = self.world.has_component(ent, game.component.container.GUTContained)
+            pos_x = pos_y = None
+            can_see_now = can_see_prev = seen = False
+
+            if position is not None:
+                pos_x = position.x
+                pos_y = position.y
+                can_see_now = self.world.map.fov[position.y, position.x]
+
+            color = "#00FFFFFF"
+            if renderable.last_seen_x is not None:
+                seen = True
+                can_see_prev = self.world.map.fov[renderable.last_seen_y, renderable.last_seen_x]
+
+            # if we can see it now, draw it and update seen pos
+            if can_see_now:
+                color = "#FFFFFFFF"
+                renderable.last_seen_y = position.y
+                renderable.last_seen_x = position.x
+            # else if we can see where it last was, forget where we've seen it and don't draw it
+            elif can_see_prev:
+                renderable.last_seen_y = None
+                renderable.last_seen_x = None
+            # else if we've seen it, draw it faded where we last saw it
+            elif seen:
+                color = "#60FFFFFF"
+                pos_y = renderable.last_seen_y
+                pos_x = renderable.last_seen_x
+            # else don't draw it
+            else:
+                continue
+            blt.layer(renderable.layer)
+            blt.color(color)
+            category, name = renderable.tile_id
+            tile_id = game.utils.render.TileCache.get(category, name)
+            blt.put(pos_x - viewport_x, pos_y - viewport_y, tile_id)
+
+    def _draw_map(self, viewport_x, viewport_y):
         for draw_x in range(self.width):
             x: int = draw_x + viewport_x
             if x >= self.world.map.width:
@@ -101,12 +169,6 @@ class BearLibRender(esper.Processor):
 
                 blt.layer(_render_layer_from_tile_type(tile_type))
                 blt.put(draw_x, draw_y, tile_id)
-
-        blt.layer(player_data.layer)
-        blt.color(player_data.color)
-        blt.put(center_x, center_y, player_data.tile_id)
-
-        blt.refresh()
 
     def _get_player_render_data(self) -> game.types.PlayerRenderData:
         # TODO: handle more than one player controlled object?
