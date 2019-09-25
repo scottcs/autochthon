d288b3cca3c8791658059a65aa924b52de13af24
scott <1170636+scottcs@users.noreply.github.com>
2019-09-25 09:54:39 -0700

    Disable some World overrides for now
    to see if they are still neeeded... they add a lot of overhead

--- game/core/world.py
+++ game/core/world.py
@@ -146,49 +146,49 @@ class World(esper.World):
             if other_pos.x == x and other_pos.y == y:
                 yield ent
 
-    def _get_component(self, component_type: typing.Any) -> typing.Any:
-        """Get an iterator for Entity, Component pairs.
-
-        :param component_type: The Component type to retrieve.
-        :return: An iterator for (Entity, Component) tuples.
-        """
-        entity_db = self._entities
-        players = set()
-
-        for entity in self._components.get(game.component.player.Player, []):
-            players.add(entity)
-            try:
-                yield entity, entity_db[entity][component_type]
-            except KeyError:
-                pass
-        for entity in self._components.get(component_type, []):
-            if entity not in players:
-                yield entity, entity_db[entity][component_type]
-
-    def _get_components(self, *component_types: typing.Any) -> typing.Any:
-        """Get an iterator for Entity and multiple Component sets.
-
-        :param component_types: Two or more Component types.
-        :return: An iterator for Entity, (Component1, Component2, etc)
-        tuples.
-        """
-
-        entity_db = self._entities
-        comp_db = self._components
-        players = set()
-
-        for entity in self._components.get(game.component.player.Player, []):
-            players.add(entity)
-            try:
-                yield entity, [entity_db[entity][ct] for ct in component_types]
-            except KeyError:
-                pass
-        try:
-            for entity in set.intersection(*[comp_db[ct] for ct in component_types]):
-                if entity not in players:
-                    yield entity, [entity_db[entity][ct] for ct in component_types]
-        except KeyError:
-            pass
+    # def _get_component(self, component_type: typing.Any) -> typing.Any:
+    #     """Get an iterator for Entity, Component pairs.
+    #
+    #     :param component_type: The Component type to retrieve.
+    #     :return: An iterator for (Entity, Component) tuples.
+    #     """
+    #     entity_db = self._entities
+    #     players = set()
+    #
+    #     for entity in self._components.get(game.component.player.Player, []):
+    #         players.add(entity)
+    #         try:
+    #             yield entity, entity_db[entity][component_type]
+    #         except KeyError:
+    #             pass
+    #     for entity in self._components.get(component_type, []):
+    #         if entity not in players:
+    #             yield entity, entity_db[entity][component_type]
+    #
+    # def _get_components(self, *component_types: typing.Any) -> typing.Any:
+    #     """Get an iterator for Entity and multiple Component sets.
+    #
+    #     :param component_types: Two or more Component types.
+    #     :return: An iterator for Entity, (Component1, Component2, etc)
+    #     tuples.
+    #     """
+    #
+    #     entity_db = self._entities
+    #     comp_db = self._components
+    #     players = set()
+    #
+    #     for entity in self._components.get(game.component.player.Player, []):
+    #         players.add(entity)
+    #         try:
+    #             yield entity, [entity_db[entity][ct] for ct in component_types]
+    #         except KeyError:
+    #             pass
+    #     try:
+    #         for entity in set.intersection(*[comp_db[ct] for ct in component_types]):
+    #             if entity not in players:
+    #                 yield entity, [entity_db[entity][ct] for ct in component_types]
+    #     except KeyError:
+    #         pass
 
     def try_component(self, entity: game.types.Entity, component_type: typing.Any) -> typing.Any:
         """Try to get a single component type for an Entity.
