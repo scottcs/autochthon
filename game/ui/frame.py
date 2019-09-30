a16ffc173488be6e97a0f0f806dce5d56ba37816
scott <1170636+scottcs@users.noreply.github.com>
2019-09-30 09:49:10 -0700

    Implement widget

--- game/ui/frame.py
+++ game/ui/frame.py
@@ -1,37 +1,23 @@
 """Draw a UI Frame."""
 import typing
 
-import game.data
 import game.render
-import game.types
-import game.utils.geometry
+import game.ui.widget
 
 
-class Frame:
+class Frame(game.ui.widget.Widget):
     """Generic UI Frame."""
 
-    def __init__(self, rect: game.utils.geometry.Rect, style: typing.Optional[str] = None) -> None:
-        self.rect: game.utils.geometry.Rect = rect
+    def __init__(self, *args, style: typing.Optional[str] = None) -> None:
+        super().__init__(*args)
         self.style: typing.Optional[str] = style
-        self.visible: bool = False
 
-    def show(self) -> None:
-        """Show the frame."""
-        self.visible = True
-
-    def hide(self) -> None:
-        """Hide the frame."""
-        self.visible = False
-
-    def render(self, renderer: game.render.BaseRenderer, layer: int) -> None:
-        """Render the frame."""
-        if not self.visible:
-            return
+    def _paint(self, renderer: game.render.BaseRenderer, layer: int) -> None:
         if self.style:
-            self._render_background(renderer, layer)
-            self._render_foreground(renderer, layer + 1)
+            self._paint_background(renderer, layer)
+            self._paint_foreground(renderer, layer + 1)
         else:
-            self._render_foreground(renderer, layer)
+            self._paint_foreground(renderer, layer)
 
     def _get_style_tile_id(self, x: int, y: int):
         if self.style is None:
@@ -59,97 +45,13 @@ class Frame:
             direction = "s"
         return game.render.TileCache.get("interface", self.style, direction=direction)
 
-    def _render_background(self, renderer: game.render.BaseRenderer, layer: int) -> None:
+    def _paint_background(self, renderer: game.render.BaseRenderer, layer: int) -> None:
         renderer.clear_layer(layer, rect=self.rect)
         for x in range(self.rect.w):
             for y in range(self.rect.h):
                 tile_id = self._get_style_tile_id(x, y)
                 renderer.draw_on_layer(layer, self.rect.x1 + x, self.rect.y1 + y, tile_id)
 
-    def _render_foreground(self, renderer: game.render.BaseRenderer, layer: int) -> None:
+    def _paint_foreground(self, renderer: game.render.BaseRenderer, layer: int) -> None:
         renderer.clear_layer(layer, rect=self.rect)
         renderer.draw_text_on_layer(layer, 4, 4, "TEST")
-
-
-class Layout:
-    """Base Layout."""
-
-    def __init__(self, rect: game.utils.geometry.Rect) -> None:
-        self._rect: game.utils.geometry.Rect = rect
-        self.spacing: int = 2
-        self.margin: int = 2
-        self.frames: typing.List[Frame] = []
-        self._visible: bool = False
-
-    def show(self):
-        """Show all child frames."""
-        self._visible = True
-        for frame in self.frames:
-            frame.visible = True
-
-    def hide(self):
-        """Hide all child frames."""
-        self._visible = False
-        for frame in self.frames:
-            frame.visible = False
-
-    def append(self, frame: Frame) -> None:
-        """Append a frame to this layout."""
-        self.frames.append(frame)
-        frame.visible = self._visible
-        self._calculate_frame_positions()
-
-    def append_space(self, amount: int) -> None:
-        """Append some blank space to the layout."""
-        self.frames.append(Frame(game.utils.geometry.Rect(0, 0, amount, amount)))
-
-    def move_to(self, x: int, y: int) -> None:
-        """Move to the given coordinates."""
-        self._rect.move_to(x, y)
-        self._calculate_frame_positions()
-
-    def render(self, renderer: game.render.BaseRenderer, layer: int) -> None:
-        """Render the frames in this layout, clipping to the size of the layout."""
-        if not self._visible:
-            return
-        # TODO: clip?
-        for frame in self.frames:
-            frame.render(renderer, layer)
-
-    def _calculate_frame_positions(self) -> None:
-        """Calculate the positions for each child frame, left to right, top to bottom."""
-        # This is the default base layout and will stack all frames on top of each other.
-        for frame in self.frames:
-            frame.rect.move_to(self._rect.x1 + self.spacing, self._rect.y1 + self.spacing)
-
-
-class VerticalLayout(Layout):
-    """Align child elements vertically."""
-
-    def _calculate_frame_positions(self):
-        x = self._rect.x1 + self.spacing
-        y = self._rect.y1
-        for frame in self.frames:
-            y += self.spacing
-            frame.rect.move_to(x, y)
-            y += frame.rect.h
-
-    def append_space(self, amount: int) -> None:
-        """Append some vertical blank space to the layout."""
-        self.frames.append(Frame(game.utils.geometry.Rect(0, 0, 1, amount)))
-
-
-class HorizontalLayout(Layout):
-    """Align child elements horizontally."""
-
-    def _calculate_frame_positions(self):
-        x = self._rect.x1
-        y = self._rect.y1 + self.spacing
-        for frame in self.frames:
-            x += self.spacing
-            frame.rect.move_to(x, y)
-            x += frame.rect.w
-
-    def append_space(self, amount: int) -> None:
-        """Append some vertical blank space to the layout."""
-        self.frames.append(Frame(game.utils.geometry.Rect(0, 0, amount, 1)))
